7750833c057cd6a856bfd8cf6e2f93de0d813d5e
Will Sun <42923017+muirheadmaster@users.noreply.github.com>
2021-04-30 02:06:50 -0400

    train and steer_model

--- train.py
+++ train.py
@@ -24,6 +24,31 @@ K.set_image_data_format('channels_first')
 IMG_WIDTH = 80
 IMG_HEIGHT = 60
 
+def mask_edges(img):
+    ignore_mask_color = 255
+    mask = np.zeros_like(img)
+    imshape = img.shape
+    # vertices of a mask polygon
+    lower_left = [0, imshape[0]]
+    lower_right = [imshape[1], imshape[0]]
+    top_left = [0, 0]
+    top_right = [imshape[1], 0]
+    top_middle = [imshape[1]/2, 0]
+    center = [imshape[1]/2, imshape[0]/2]
+    left_middle = [0, imshape[0]/2]
+    right_middle = [imshape[1], imshape[0]/2]
+    vertices = [np.array(
+        [top_left, left_middle, right_middle, top_right],
+        dtype=np.int32)]
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    copied_img = np.copy(img)
+    pts = np.array([vertices], np.int32)
+    pts = pts.reshape((-1, 1, 2))
+    cv2.polylines(copied_img, [pts], True, (255, 255, 255))
+    # cv2.imshow(?mask region?, copied_img)
+    img = cv2.bitwise_and(img, mask)
+    return img
+
 def preprocess_img(img):
     #This function should be consistent with 'preprocess_img' in ml_driver.py
     img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))    
@@ -32,6 +57,12 @@ def preprocess_img(img):
     #################
     #Task (optional): Any pre-processing?
     #################
+    # mask
+    img = mask_edges(img)
+    # threshold
+    _, img = cv2.threshold(img, 140, 255, cv2.THRESH_BINARY)
+    # flip
+    img = cv2.flip(img, 1)
 
     return img
 
@@ -56,6 +87,7 @@ def load_dataset(image_path):
         #Task (optional): Data augmentation?
         #################
 
+
     X = np.array(X, dtype='float32')
     Y = np.array(Y, dtype='float32')
     X = X/255.0
@@ -70,20 +102,34 @@ def create_model():
     #################
     #Task: Create your own model
     #################
-    model.add(Conv2D(24, (5, 5), padding='same',
+    model.add(Conv2D(32, (3, 2), padding='same',
                 input_shape=(1, IMG_WIDTH, IMG_HEIGHT),
                 activation='relu'))
-    model.add(Conv2D(36, (5, 5), padding='same',
+    model.add(Conv2D(32, (3, 3), padding='same',
                 activation='relu'))
-    model.add(Conv2D(48, (3, 3), padding='same',
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(64, (3, 3), padding='same',
                 activation='relu'))
     model.add(Conv2D(64, (3, 3), padding='same',
                 activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Conv2D(128, (3, 3), padding='same',
+                activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same',
+                activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    
     model.add(Flatten())
-    #model.add(Dense(1164, activation='relu'))
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(10, activation='relu'))
+
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(1))
 
     return model
